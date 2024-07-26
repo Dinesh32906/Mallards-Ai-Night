6f496ccd852531cdafcd6f36db3557d4a87e2db8
Dinesh32906 <54185190+Dinesh32906@users.noreply.github.com>
2024-07-26 12:21:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -149,12 +149,18 @@ def complete(myquestion, session):
 def main():
     init_session_state()
 
-    hide_github_icon = """
-    #GithubIcon {
-      visibility: hidden;
-    }
+    st.markdown(
     """
-    st.markdown(hide_github_icon, unsafe_allow_html=True)
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
 
     # Adding custom HTML and CSS
     # Adding custom HTML and CSS
